986d19a66cbc2da5f27f56b8a47086d1cf3d0e0a
Nando <mnandohidayat@gmail.com>
2018-12-31 19:45:01 +0700

    fix checkout did not reset price

--- app/src/main/java/com/nandohidayat/app/ayamku/Checkout.java
+++ app/src/main/java/com/nandohidayat/app/ayamku/Checkout.java
@@ -2,15 +2,25 @@ package com.nandohidayat.app.ayamku;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Checkout extends AppCompatActivity {
     EditText totalPrice, payAmount, change;
@@ -53,10 +63,52 @@ public class Checkout extends AppCompatActivity {
     }
 
     public void pay(View view) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            OkHttpClient client = new OkHttpClient();
+
+            HttpUrl.Builder builder = HttpUrl.parse("http://ayam-ku-nandohidayat.c9users.io/api/jual.php").newBuilder();
+            builder.addQueryParameter("username", SplashActivity.sh.getString("username", null));
+            builder.addQueryParameter("total", price + "");
+            builder.addQueryParameter("bayar", pay + "");
+
+            String url = builder.build().toString();
+
+            Request request = new Request.Builder().url(url).build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, final Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                if(response.body().string().contains("success")) {
+                                    Toast.makeText(Checkout.this, "Payment Successfull", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(Checkout.this, "Payment Failed, Please Try Again", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SplashActivity.editor.putFloat("price", 0.0f);
         SplashActivity.editor.commit();
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
-        Toast.makeText(getApplicationContext(), "Paid", Toast.LENGTH_LONG).show();
     }
 }
