97fb8eb019afcb1bb7f2d78651a3d7900384cc16
Nando <mnandohidayat@gmail.com>
2019-01-01 11:55:58 +0700

    add Nota Pembelian by opening browser

--- app/src/main/java/com/nandohidayat/app/ayamku/Checkout.java
+++ app/src/main/java/com/nandohidayat/app/ayamku/Checkout.java
@@ -1,6 +1,7 @@
 package com.nandohidayat.app.ayamku;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
@@ -11,6 +12,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -69,7 +74,7 @@ public class Checkout extends AppCompatActivity {
 
             OkHttpClient client = new OkHttpClient();
 
-            HttpUrl.Builder builder = HttpUrl.parse("http://ayam-ku-nandohidayat.c9users.io/api/jual.php").newBuilder();
+            HttpUrl.Builder builder = HttpUrl.parse("http://ayam-ku-nandohidayat.c9users.io/api/jual/read.php").newBuilder();
             builder.addQueryParameter("username", SplashActivity.sh.getString("username", null));
             builder.addQueryParameter("total", price + "");
             builder.addQueryParameter("bayar", pay + "");
@@ -90,10 +95,21 @@ public class Checkout extends AppCompatActivity {
                         @Override
                         public void run() {
                             try {
-                                if(response.body().string().contains("success")) {
-                                    Toast.makeText(Checkout.this, "Payment Successfull", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(Checkout.this, "Payment Failed, Please Try Again", Toast.LENGTH_LONG).show();
+                                try {
+                                    String data = response.body().string();
+                                    JSONArray jsonArray = new JSONArray(data);
+                                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+                                    String status = jsonObject.getString("status");
+
+                                    if(status.contains("success")) {
+                                        Toast.makeText(Checkout.this, "Payment Successfull", Toast.LENGTH_LONG).show();
+                                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(jsonObject.getString("pdf")));
+                                        startActivity(browserIntent);
+                                    } else {
+                                        Toast.makeText(Checkout.this, "Payment Failed, Please Try Again", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
                             } catch (IOException e) {
                                 e.printStackTrace();
